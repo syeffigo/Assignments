402e444d41912c2ab8ac5e4a719f8497d3f6d3c6
Saurabh Yadav <saurabh.yadav@effigo.tech>
2024-02-01 15:03:11 +0530

    Adding Comments

--- Assignment1.java
+++ Assignment1.java
@@ -9,6 +9,7 @@ import java.util.Date;
 
 public class Assignment1 {
       
+    /*  ============= CONVERTING TO UNIX TIMESTAMP ============== */
     private static long convertToUnixTimestamp(String userInput) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         try {
@@ -20,17 +21,18 @@ public class Assignment1 {
         }
     }
 
-    
+    /* ================= CONVERTING LOCAL TIMESTAMP TO UTC ================== */
     private static ZonedDateTime convertToZonedDateTime(String userInput, String zoneId) {
         LocalDateTime localDateTime = LocalDateTime.parse(userInput, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
         return localDateTime.atZone(ZoneId.of(zoneId));
     }
 
-    
+    /* ================ CONVERTING UTC TO LOCAL TIMESTAMP =================== */
     private static ZonedDateTime convertUTCtoIST(ZonedDateTime utcDateTime) {
         return utcDateTime.withZoneSameInstant(ZoneId.of("Asia/Kolkata"));
     }
 
+    /* =============== METHOD TO FORMAT ZONED-DATE-TIME =================== */
     private static String formatZonedDateTime(ZonedDateTime zonedDateTime) {
         return zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ssXXX"));
     }
@@ -38,15 +40,18 @@ public class Assignment1 {
     public static void main(String[] args) {
         
         String userInput = "29/01/2024 01:00";
-
+       
+        /* ============= CONVERTING USER INPUT TO UNIX TIMESTAMP ================ */
         long unixTimestamp = convertToUnixTimestamp(userInput);
         System.out.println("Unix Timestamp (Local IST): " + unixTimestamp);
 
+        /* ============= CONVERTING LOCAL TIMESTAMP TO UTC ================= */
         ZonedDateTime istDateTime = convertToZonedDateTime(userInput, "Asia/Kolkata");
         ZonedDateTime utcDateTime = istDateTime.withZoneSameInstant(ZoneId.of("UTC"));
         System.out.println("Local IST Timestamp: " + formatZonedDateTime(istDateTime));
         System.out.println("UTC Timestamp: " + formatZonedDateTime(utcDateTime));
-
+        
+        /* ============ CONVERTING UTC TO LOCAL TIMESTAMP ===================== */
         ZonedDateTime convertedIstDateTime = convertUTCtoIST(utcDateTime);
         System.out.println("Converted IST Timestamp: " + formatZonedDateTime(convertedIstDateTime));
     }
